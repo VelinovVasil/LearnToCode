5141d591fc13d0beec0366356da5ead0350cbc25
danie <danielnikolaevyordanov@gmail.com>
2024-02-12 10:48:59 +0200

    Reply Model

--- src/main/java/org/example/events/npmg/models/Reply.java
+++ src/main/java/org/example/events/npmg/models/Reply.java
@@ -2,13 +2,17 @@ package org.example.events.npmg.models;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.events.npmg.models.Role.Role;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Data
@@ -16,27 +20,36 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "replies")
+@Table(name="replies")
+
+
 public class Reply {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @NotEmpty(message = "Reply to the question is mandatory")
+    @NotEmpty(message = "Reply to the question is manditory")
     @Column(nullable = false)
-    private String reply;
+    private String replyText;
+
 
     @Column(name = "time_published")
     private LocalDateTime dateOfCreation;
 
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "reply_to_reply_id", nullable = false)
+    private Reply replyToReply;
 
     @PrePersist
     protected void onCreate() {
