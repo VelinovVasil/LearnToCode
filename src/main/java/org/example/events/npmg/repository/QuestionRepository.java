5619cc0f1bcec4ef6e0c6a5ac8f070acc57cc3aa
Vasil Velinov <velinov.vasil.i@gmail.com>
2024-02-13 12:05:07 +0200

    Added TODOs in the Repository interfaces.

--- src/main/java/org/example/events/npmg/repository/QuestionRepository.java
+++ src/main/java/org/example/events/npmg/repository/QuestionRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    // TODO
 
 }
--- src/main/java/org/example/events/npmg/repository/TagRepository.java
+++ src/main/java/org/example/events/npmg/repository/TagRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-
+    // TODO
 
 }
